d28db2d20001862efb74a3a9c391023222830f00
Evaldas Latoskinas <E.latoskinas@student.tudelft.nl>
2019-03-20 23:22:14 +0100

    MappingTests commenting

--- src/main/java/mongoTesting/MappingTests.java
+++ src/main/java/mongoTesting/MappingTests.java
@@ -12,20 +12,23 @@ import java.util.ArrayList;
 public class MappingTests {
     public static void main(String[] args) {
         MongoClient mongoClient = new MongoClient("localhost");
-
         Morphia morphia = new Morphia();
-
         Datastore datastore = morphia.createDatastore(mongoClient, "testdb");
 
-        OntologyTerm term1 = new OntologyTerm("term1", "Term 1", "Term 1 Description", "namespace", false);
+        // Save some OntologyTerms to the database
+        OntologyTerm term1 = new OntologyTerm("term1", "Term 1", "Term 1 Description",
+                "namespace", false);
         datastore.save(term1);
 
-        OntologyTerm term2 = new OntologyTerm("term2", "Term 2", "Term 2 Description", "namespace", false);
+        OntologyTerm term2 = new OntologyTerm("term2", "Term 2", "Term 2 Description",
+                "namespace", false);
         datastore.save(term2);
 
+        // Save an OntologyRelation to the database
         OntologyRelation relation = new OntologyRelation(term1, term2, "relationship", true, false);
         datastore.save(relation);
 
+        // Construct & save a list of Publications
         ArrayList<Publication> publications = new ArrayList<Publication>();
 
         Publication publication1 = new Publication(52, "Publication", "3", "100", 2019);
@@ -38,9 +41,11 @@ public class MappingTests {
 
         datastore.save(publications);
 
+        // Construct & save an Author object
         Author author = new Author("John", "Doe", "John Doe", "jdie", publications);
         datastore.save(author);
 
+        // Construct & save an OntologyEvidence object
         OntologyAnnotationEvidenceCode code = new OntologyAnnotationEvidenceCode("code", "name", "url");
         OntologyEvidence ontologyEvidence = new OntologyEvidence(code);
 
@@ -49,12 +54,19 @@ public class MappingTests {
         OntologyTerm term3 = new OntologyTerm("term3", "Term 3", "Term 3 Description", "namespace", false);
         datastore.save(term3);
 
+        // --- Simple query examples ---
+        // Query to find OntologyTerm with id equal to term3
         Query<OntologyTerm> query = datastore.createQuery(OntologyTerm.class).field("_id").equal("term3");
 //        datastore.findAndDelete(query);
 
-        UpdateOperations<OntologyTerm> update = datastore.createUpdateOperations(OntologyTerm.class).set("description", "Term 3 Description Updated!");
+        // Update query to update OntologyTerm descrption to "Term 3 Description Updated!"
+        UpdateOperations<OntologyTerm> update = datastore.createUpdateOperations(OntologyTerm.class).set("description",
+                "Term 3 Description Updated!");
+
+        // Execute update query on OntologyTerm with _id equal to term3
         datastore.update(query, update);
 
+        // Remove description field query
         UpdateOperations<OntologyTerm> update2 = datastore.createUpdateOperations(OntologyTerm.class).unset("description");
         datastore.update(query, update2);
     }
